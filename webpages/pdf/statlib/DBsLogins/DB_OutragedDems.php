6cca86e30c46063ef01b1632e3e4e2ff007923e6
Theo Chino <theo@outrageddems.nyc>
2018-02-01 01:39:12 -0500

    Added another placeholrder file

---
+++ webpages/pdf/statlib/DBsLogins/DB_OutragedDems.php
@@ -0,0 +1,13 @@
+<?php
+$databasename = "<DATABASE NAME>";
+$databaseserver = "<DATABASE HOST>";
+$databaseport = 3306;
+$databaseuser = "<USERNAME>";
+$databasepassword = "<PASSWORD>";
+
+$DBErrorsFilename = "/tmp/DBErrors_" . $_SERVER["SERVER_ADDR"] . ".txt";
+
+$sslkeys["srvkey"] = $_SERVER["DOCUMENT_ROOT"] . "<PATH TO SSL KEYS>/client-mysql-key.pem";
+$sslkeys["srvcert"] = $_SERVER["DOCUMENT_ROOT"] . "<PATH TO SSL KEYS>/client-mysql-cert.pem";
+$sslkeys["srvca"] = $_SERVER["DOCUMENT_ROOT"] . "<PATH TO SSL KEY>/ca-mysql-cert.pem";
+?>
