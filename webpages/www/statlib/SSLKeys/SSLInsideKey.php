9a3aab27efc16f597aa069aea5818295e928363d
Theo Chino <github@theochino.com>
2018-02-28 03:09:48 -0500

    Update SSLInsideKey.php
    Added instructions to create the keys.

--- webpages/www/statlib/SSLKeys/SSLInsideKey.php
+++ webpages/www/statlib/SSLKeys/SSLInsideKey.php
@@ -1,6 +1,9 @@
 <?php
 
 // You need to create a private and public key with SSL
+// To create the private key: openssl genrsa -out private.pem 2048
+// To create the public key: openssl rsa -in private.pem -outform PEM -pubout -out public.pem
+// Then copy each string into the correct variable.
 
 $PrivKey =      "-----BEGIN RSA PRIVATE KEY-----\n" .
                 "MIIxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n" .
